bd4f9ec195f7c0784c6544e83cf12266fb22bcbd
gjakubik <gavito8800@gmail.com>
2021-10-13 13:04:37 -0400

    ,more

--- full_combiner.py
+++ full_combiner.py
@@ -23,7 +23,7 @@ def main():
         indicies = [i for i in range(start, end)]
         df["INDEX"] = indicies
         df.set_index("INDEX", inplace=True)
-        print(df["INDEX"])
+        print(df.index)
         start = end
 
     # TODO: then concatenate all their dataframes
